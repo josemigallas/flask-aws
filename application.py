343a3eb3ce1ff430813676ba9776f099a849a11e
josemigallas <josemigallas@gmail.com>
2021-02-05 15:17:42 +0100

    🔖  0.0.1

--- application.py
+++ application.py
@@ -1,18 +1,30 @@
 from flask import Flask
 
+VERSION = '0.0.1'
+
 # print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
 
 # some bits of text for the page.
 header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+<html>
+  <head>
+    <title>EB Flask Test</title>
+  </head>
+  <body>
+'''
 instructions = '''
     <p><em>Hint</em>: This is a RESTful web service! Append a username
     to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
+    someone specific.</p>
+'''
 home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+footer_text = f'''
+    </body>
+  <footer>Version {VERSION}</footer>
+</html>
+'''
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
